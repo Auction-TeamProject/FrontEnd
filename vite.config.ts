347b9ff39894acc35469c6efc0e8e7525e3e98f5
FE 유규상 <yugyusang12@gmail.com>
2024-12-28 21:04:23 +0900

    chore: PWA 플러그인 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: '앱 이름',
+        short_name: '앱 짧은 이름',
+        description: '앱 설명',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
