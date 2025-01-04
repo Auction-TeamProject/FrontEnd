31772c6b7ba2c4736eca3e48d2c025929138475d
FE 유규상 <yugyusang12@gmail.com>
2025-01-04 17:41:47 +0900

    feat: 아이템 상세 페이지 컴포넌트 추가

---
+++ src/pages/ItemDetailPage.tsx
@@ -0,0 +1,128 @@
+import { useEffect, useState } from 'react';
+import { useOutletContext, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+import ItemBids from '../components/itemDetail/ItemBids';
+import ItemDescription from '../components/itemDetail/ItemDescription';
+import { LayoutContextType } from '../components/layout/HeaderBarLayout';
+import {
+  MarginLessContainer,
+  MarginLessContainerTitle,
+} from '../styles/commonStyle';
+
+export type BidType = { date: Date; description: string; bidder: string };
+
+type ItemDetailData = {
+  itemName: string;
+  itemDescription: string;
+  bids: Array<BidType>;
+  photoUrl: string[];
+  viewerCount: number;
+  bidderCount: number;
+  endDate: Date;
+} | null;
+
+const ItemDetailPage = () => {
+  const { itemId } = useParams<{ itemId: string }>();
+  const [itemDetails, setItemDetails] = useState<ItemDetailData>(null);
+  const { setHeaderTitle, dropDownArray } =
+    useOutletContext<LayoutContextType>();
+
+  //헤더 타이틀 설정 테스트
+  useEffect(() => {
+    setHeaderTitle('test');
+    return () => {
+      setHeaderTitle('');
+    };
+  }, [setHeaderTitle]);
+
+  // 드롭다운 메뉴 설정 테스트
+  useEffect(() => {
+    dropDownArray.splice(0, dropDownArray.length);
+    dropDownArray.push({
+      name: '테스트3',
+      callback: () => {
+        console.log('테스트3');
+      },
+    });
+    return () => {
+      dropDownArray.splice(0, dropDownArray.length);
+    };
+  }, [dropDownArray]);
+
+  useEffect(() => {
+    // 가상 api 호출
+    setItemDetails({
+      itemName: itemId || '상품명',
+      itemDescription: '상품 설명',
+      photoUrl: [],
+      viewerCount: 0,
+      bidderCount: 0,
+      bids: [
+        {
+          date: new Date(),
+          description: '테스트1231231232312312331',
+          bidder: '테스트1123',
+        },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+        { date: new Date(), description: '테스트', bidder: '테스트' },
+      ],
+      endDate: new Date(),
+    });
+  }, [itemId]);
+
+  return (
+    <PageContainer>
+      <MarginLessContainer $direction="row">
+        <MarginLessContainerTitle>상품명</MarginLessContainerTitle>
+        <ItemName>{itemDetails?.itemName}</ItemName>
+      </MarginLessContainer>
+      <ItemDescription
+        itemDescription={itemDetails?.itemDescription || ''}
+        photoUrl={itemDetails?.photoUrl || []}
+        viewerCount={itemDetails?.viewerCount || 0}
+      />
+      <ItemBids
+        bids={itemDetails?.bids || []}
+        bidderCount={itemDetails?.bidderCount || 0}
+        endDate={itemDetails?.endDate || new Date()}
+      />
+    </PageContainer>
+  );
+};
+
+export default ItemDetailPage;
+
+const PageContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: var(--item-detail-page-gap);
+  width: 100%;
+  height: 100%;
+  margin-top: var(--item-detail-page-gap);
+
+  --item-detail-page-gap: 8px;
+`;
+
+const ItemName = styled.p`
+  padding: 0;
+  margin: 0;
+  margin-left: 1rem;
+  font-size: 1rem;
+`;
