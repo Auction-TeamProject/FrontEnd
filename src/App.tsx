015d7c5596d7739035986a049b9011637b3fb35a
FE 유규상 <yugyusang12@gmail.com>
2025-01-09 17:46:09 +0900

    feat: 새로고침시 사용자 정보를 로드하고 ToastProvider로 앱 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
 
 import DropDownHeaderBarLayout from './components/layout/DropDownHeaderBarLayout';
+import { ToastProvider } from './components/Modal/ToastProvider';
+import { useUserActions } from './context/userStore';
 import ItemDetailPage from './pages/ItemDetailPage';
 
 const router = createBrowserRouter([
@@ -35,10 +38,19 @@ const router = createBrowserRouter([
     element: <div>잘못된 경로입니다</div>,
   },
 ]);
+
 const App = () => {
+  const { loadUser, setUser } = useUserActions();
+  useEffect(() => {
+    setUser('작성자'); //원래 로그인시 사용해야하지만 테스트를 위해 사용
+    loadUser();
+  }, [loadUser, setUser]);
+
   return (
     <>
-      <RouterProvider router={router} />
+      <ToastProvider>
+        <RouterProvider router={router} />
+      </ToastProvider>
     </>
   );
 };
