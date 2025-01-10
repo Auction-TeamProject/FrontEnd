3c9dd092a51b95e0ac9cdb7a9a1916f8545f190e
FE 유규상 <yugyusang12@gmail.com>
2025-01-10 23:57:22 +0900

    feat: 폼 스타일 컴포넌트 추가

--- src/styles/commonStyle.ts
+++ src/styles/commonStyle.ts
@@ -53,3 +53,57 @@ export const StyledInput = styled.input`
   background-color: var(--background-color);
   border-radius: 10px;
 `;
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const ErrorDescription = styled.p`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  height: 1rem;
+  font-size: var(--font-size-small);
+  font-weight: normal;
+  color: var(--muted-text-color);
+`;
+
+export const InputSection = styled.section`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 10%;
+`;
+
+export const StyledLabel = styled.label`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  font-weight: bold;
+`;
+
+export const InputContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const FlexibleBarButton = styled(BarButton)`
+  flex: 1;
+`;
