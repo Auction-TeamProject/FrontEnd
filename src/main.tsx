3eab7493ddabc4eaf5287331a8441574ffaa1aff
FE 유규상 <yugyusang12@gmail.com>
2025-01-12 17:46:53 +0900

    feat: React Query를 위한 QueryClientProvider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 import GlobalStyle from './styles/globalStyle.ts';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStyle />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
