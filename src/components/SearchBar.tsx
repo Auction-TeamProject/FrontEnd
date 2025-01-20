43627e027594b3f6b89485a0af9c8adf19f93b23
FE 유규상 <yugyusang12@gmail.com>
2025-01-20 20:58:36 +0900

    feat: 검색 바 컴포넌트 추가

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,46 @@
+import { Dispatch, HTMLAttributes, SetStateAction, useRef } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import styled from 'styled-components';
+
+type SearchBarProps = {
+  setSearchInputValue: Dispatch<SetStateAction<string | undefined>>;
+} & HTMLAttributes<HTMLDivElement>;
+
+const SearchBar = ({ setSearchInputValue, ...props }: SearchBarProps) => {
+  const SearchInputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <SearchBarContainer {...props}>
+      <SearchInput ref={SearchInputRef}></SearchInput>
+      <SearchButtonIcon
+        onClick={() => setSearchInputValue(SearchInputRef.current?.value)}
+      />
+    </SearchBarContainer>
+  );
+};
+
+export default SearchBar;
+
+const SearchBarContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 3rem;
+  padding: 0 0 0 1rem;
+  background-color: var(--background-color);
+  border-radius: 1rem;
+`;
+
+const SearchInput = styled.input`
+  position: relative;
+  flex: 1;
+`;
+
+const SearchButtonIcon = styled(FaSearch)`
+  position: relative;
+  margin: 0 1rem;
+  cursor: pointer;
+`;
